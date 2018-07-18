7a1daf3e0233c1673fa1f96102f79da76a00014b
mspink <arpinkchen@gmail.com>
2018-07-18 20:28:08 +0800

    add controllers and home page

---
+++ app/controllers/admin/bulletins_controller.rb
@@ -0,0 +1,2 @@
+class Admin::BulletinsController < ApplicationController
+end
---
+++ test/controllers/admin/bulletins_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::BulletinsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
